c6374183064af3055b7264e576896c06623ecada
LandryKnight <45949848+LandryKnight@users.noreply.github.com>
2020-02-16 18:32:50 -0600

    16/02

---
+++ Personas/Requisitos Generales.php
@@ -0,0 +1,142 @@
+
+<?php  ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Requsitos Generales De Documentos</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+
+
+<p>Persona Fisica</p>
+<p>Identificación Oficial expedida por el IFE o INE</p>
+<p>CURP Clave Única de Reistro de Población</p>
+<p>Comprobante de domicilio o Constancia  de Residencia firmado por la autoridad competente</p>
+<p>Croquis de bien a adquirir</p>
+
+
+<p>Persona Moral</p>
+
+<p>Acta constitutiva de la Organización, Inscrita a el Registro Público de la Propiedad</p>
+<p>RFC Registro Federal de Contribuyentes</p>
+<p>Identificacion Oficial expedida por el IFE o el INE, Solo del Presidente</p>
+<p>RFC Registro Federal de Contribuyentes, Solo del Presidente</p>
+<p>CURP Clave Única de Reistro de Población, Solo del Presidente</p>
+<p>Comprobante de domicilio o Constancia de Residencia firmado por la autoridad competente, Solo del Presidente</p>
+<p>Croquis de localizacion del predio</p>
+<p>Cotización del bien a adquirir</p>
+
+
+<p>Declaraciones del Solicitante</p>
+
+<p>a)Que no realizo actividades productivas ni comerciales ilicitas </p>
+<p>b)Que no se aplicaran los incentivos unicamente para los fines autorizados, y en caso de no realizarlo se entregara el recurso
+asi como los productos financieros</p>
+
+<p>c)Que no he recibido o estoy recibiendo  apoyo alguno para el mismo concepto en otro programa, que implique duplicidad de incentivos</p>
+
+<p>d)Que los datos aqui expuestos son veridicos y me compromento a cumplir con los ordenamientos establecidos  en la mecanica operativa establecida</p>
+
+<p>e)Expreso mi total y cabal compromiso, para realizar las invesiones y/o trabajos que corresponden para ejecutar las acciones del proyecto que requieran</p>
+
+<p>f) Que estoy cierto que la entrega ed esta solicitud, asi como la de los documentos solicitados, no implica aceptacion u obligacion del pago de los incentivos solicitados</p>
+
+
+
+<h2>Observacines </h2>
+
+<input type="text" name="">
+
+
+<h2>Dictamen de la solicitud</h2>
+
+<p>Positiva</p>
+
+<p>Negativa</p>
+
+
+
+<table border="1">
+	
+	<tr>
+		<td>Conceptos de Apoyo Solicitado</td>
+		<td>Unidad de Medida</td>
+		<td>Cantidad Solicitada</td>
+		<td>Apoyo Estatal Solicitado (pesos) </td>
+		<td>Apoyo Municipal Solicitado (pesos) </td>
+		<td>Aportación Beneficiario (pesos)</td>
+		<td>Inversión Total (pesos)</td>
+	</tr>
+
+	<tr>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+	</tr>
+
+	<tr>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+	</tr>
+
+	<tr>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+	</tr>
+
+	<tr>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+	</tr>
+
+	<tr>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+	</tr>
+	<tr>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name="">	 </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+		<td><input type="text" name=""> </td>
+	</tr>
+
+</table>
+
+<p>Fecha </p>
+
+<input type="text" name="">
+
+
+</body>
+</html>
