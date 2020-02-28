99c1672de0d6022118106eea324ac2034c9fa1dc
LandryKnight <45949848+LandryKnight@users.noreply.github.com>
2020-02-28 00:56:36 -0600

    27/02 FInal

--- XAMPP/htdocs/Sedea/Personas/Fisica/conceptosApoyoPF.php
+++ XAMPP/htdocs/Sedea/Personas/Fisica/conceptosApoyoPF.php
@@ -1,4 +1,5 @@
 <?php 
+/* 
 require_once('conect.php');
 
 
@@ -56,6 +57,7 @@ $InsertDatoProyecto = "INSERT INTO personafisicaNP(sfolioImpreso,sNombreProyecto
 
 
 
+*/
 
 function sanitizeString($var){
 
@@ -76,98 +78,105 @@ function sanitizeString($var){
 <head>
 	<title>Conceptos de Apoyos</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
 
+<div class="conceptoApoyo">
 
 <form method="post" action="requisitosGeneralesPF.php">
 
 
 <input type="hidden" name="IfolioImpreso" value="<?php echo $folioImpreso; ?>">
+
+
+<h1>Conceptos de Apoyo para Persona Física</h1>
 <table border="1">
 	
 	<tr>
 		<td>Conceptos de <br>Apoyo Solicitado</td> 
-		<td>Unidad de <br>Medida</td>   
+		<td class="UnidadMedida">Unidad de <br>Medida</td>   
 		<td>Cantidad <br>Solicitada</td> 
 		<td>Apoyo Estatal <br>Solicitado (pesos) </td> 
 		<td>Apoyo Municipal <br>Solicitado (pesos) </td> 
 		<td>Aportación Beneficiario (pesos)</td> 
-		<td>Inversión Total (pesos)</td>  
+	
 	</tr>
 
 	<tr>
-		<td><input type="text" name="ApoyoSolicitado1" maxlength="35" 	placeholder="Estufa ecológica" required="required"> </td>
-		<td><input type="text" name="UniMedida1" maxlength="5" 	placeholder="2" required="required">  </td>
-		<td><input type="text" name="CanSolicitada1" maxlength="5" 	placeholder="2" required="required">  </td>
-		<td><input type="text" name="ApoyoEstatalSolicitado1" maxlength="20" 	placeholder="$500,000" required="required"> </td>
-		<td><input type="text" name="ApoyoMunicipalSolicitado1" maxlength="20"	placeholder="$250,000" required="required"> </td>
-		<td><input type="text" name="AportacionBeneficiario1" maxlength="20" 	placeholder="$250,000" required="required"> </td>
-		<td><input type="text" name="InversionTotal1" maxlength="20"	placeholder="$1,000,000" required="required"> </td>
+		<td><input type="text" name="ApoyoSolicitado1" maxlength="35" 	placeholder=" Estufa ecológica" required="required"> </td>
+		<td class="UnidadMedida"><input type="text" name="UniMedida1" maxlength="5" 	placeholder=" 2" required="required">  </td>
+		<td><input type="text" name="CanSolicitada1" maxlength="5" 	placeholder=" 2" required="required">  </td>
+		<td><input type="text" name="ApoyoEstatalSolicitado1" maxlength="20" 	placeholder=" $500,000" required="required"> </td>
+		<td><input type="text" name="ApoyoMunicipalSolicitado1" maxlength="20"	placeholder=" $250,000" required="required"> </td>
+		<td><input type="text" name="AportacionBeneficiario1" maxlength="20" 	placeholder=" $250,000" required="required"> </td>
+		
 	</tr>
 
 	<tr>
-		<td><input type="text" name="ApoyoSolicitado2" maxlength="35" 	placeholder="Estufa ecológica" > </td>
-		<td><input type="text" name="UniMedida2" maxlength="5" 	placeholder="2" >  </td>
+		<td><input type="text" name="ApoyoSolicitado2" maxlength="35" 	placeholder=" Estufa ecológica" > </td>
+		<td class="UnidadMedida"><input type="text" name="UniMedida2" maxlength="5" 	placeholder=" 2" >  </td>
 		<td><input type="text" name="CanSolicitada2" maxlength="5" 	placeholder="2" >  </td>
 		<td><input type="text" name="ApoyoEstatalSolicitado2" maxlength="20" 	placeholder="$500,000" > </td>
 		<td><input type="text" name="ApoyoMunicipalSolicitado2" maxlength="20"	placeholder="$250,000" > </td>
 		<td><input type="text" name="AportacionBeneficiario2" maxlength="20" 	placeholder="$250,000" > </td>
-		<td><input type="text" name="InversionTotal2" maxlength="20"	placeholder="$1,000,000" > </td>
+		
 	</tr>
 
 
 
 		<tr>
-		<td><input type="text" name="ApoyoSolicitado3" maxlength="35" 	placeholder="Estufa ecológica" > </td>
-		<td><input type="text" name="UniMedida3" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="CanSolicitada3" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="ApoyoEstatalSolicitado3" maxlength="20" 	placeholder="$500,000" > </td>
-		<td><input type="text" name="ApoyoMunicipalSolicitado3" maxlength="20"	placeholder="$250,000" > </td>
-		<td><input type="text" name="AportacionBeneficiario3" maxlength="20" 	placeholder="$250,000" > </td>
-		<td><input type="text" name="InversionTotal3" maxlength="20"	placeholder="$1,000,000" > </td>
+		<td><input type="text" name="ApoyoSolicitado3" maxlength="35" 	placeholder=" Estufa ecológica" > </td>
+		<td class="UnidadMedida"><input type="text" name="UniMedida3" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="CanSolicitada3" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="ApoyoEstatalSolicitado3" maxlength="20" 	placeholder=" $500,000" > </td>
+		<td><input type="text" name="ApoyoMunicipalSolicitado3" maxlength="20"	placeholder=" $250,000" > </td>
+		<td><input type="text" name="AportacionBeneficiario3" maxlength="20" 	placeholder=" $250,000" > </td>
+		
 	</tr>
 
 		<tr>
-		<td><input type="text" name="ApoyoSolicitado4" maxlength="35" 	placeholder="Estufa ecológica" > </td>
-		<td><input type="text" name="UniMedida4" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="CanSolicitada4" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="ApoyoEstatalSolicitado4" maxlength="20" 	placeholder="$500,000" > </td>
-		<td><input type="text" name="ApoyoMunicipalSolicitado4" maxlength="20"	placeholder="$250,000" > </td>
-		<td><input type="text" name="AportacionBeneficiario4" maxlength="20" 	placeholder="$250,000" > </td>
-		<td><input type="text" name="InversionTotal4" maxlength="20"	placeholder="$1,000,000" > </td>
+		<td><input type="text" name="ApoyoSolicitado4" maxlength="35" 	placeholder=" Estufa ecológica" > </td>
+		<td class="UnidadMedida"><input type="text" name="UniMedida4" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="CanSolicitada4" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="ApoyoEstatalSolicitado4" maxlength="20" 	placeholder=" $500,000" > </td>
+		<td><input type="text" name="ApoyoMunicipalSolicitado4" maxlength="20"	placeholder=" $250,000" > </td>
+		<td><input type="text" name="AportacionBeneficiario4" maxlength="20" 	placeholder=" $250,000" > </td>
+		
 	</tr>
 
 		<tr>
-		<td><input type="text" name="ApoyoSolicitado5" maxlength="35" 	placeholder="Estufa ecológica" > </td>
-		<td><input type="text" name="UniMedida5" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="CanSolicitada5" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="ApoyoEstatalSolicitado5" maxlength="20" 	placeholder="$500,000" > </td>
-		<td><input type="text" name="ApoyoMunicipalSolicitado5" maxlength="20"	placeholder="$250,000" > </td>
-		<td><input type="text" name="AportacionBeneficiario5" maxlength="20" 	placeholder="$250,000" > </td>
-		<td><input type="text" name="InversionTotal5" maxlength="20"	placeholder="$1,000,000" > </td>
+		<td><input type="text" name="ApoyoSolicitado5" maxlength="35" 	placeholder=" Estufa ecológica" > </td>
+		<td class="UnidadMedida"><input type="text" name="UniMedida5" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="CanSolicitada5" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="ApoyoEstatalSolicitado5" maxlength="20" 	placeholder=" $500,000" > </td>
+		<td><input type="text" name="ApoyoMunicipalSolicitado5" maxlength="20"	placeholder=" $250,000" > </td>
+		<td><input type="text" name="AportacionBeneficiario5" maxlength="20" 	placeholder=" $250,000" > </td>
+		
 	</tr>
 
 		<tr>
-		<td><input type="text" name="ApoyoSolicitado6" maxlength="35" 	placeholder="Estufa ecológica" > </td>
-		<td><input type="text" name="UniMedida6" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="CanSolicitada6" maxlength="5" 	placeholder="2" >  </td>
-		<td><input type="text" name="ApoyoEstatalSolicitado6" maxlength="20" 	placeholder="$500,000" > </td>
-		<td><input type="text" name="ApoyoMunicipalSolicitado6" maxlength="20"	placeholder="$250,000" > </td>
-		<td><input type="text" name="AportacionBeneficiario6" maxlength="20" 	placeholder="$250,000" > </td>
-		<td><input type="text" name="InversionTotal6" maxlength="20"	placeholder="$1,000,000" > </td>
+		<td><input type="text" name="ApoyoSolicitado6" maxlength="35" 	placeholder=" Estufa ecológica" > </td>
+		<td class="UnidadMedida"><input type="text" name="UniMedida6" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="CanSolicitada6" maxlength="5" 	placeholder=" 2" >  </td>
+		<td><input type="text" name="ApoyoEstatalSolicitado6" maxlength="20" 	placeholder=" $500,000" > </td>
+		<td><input type="text" name="ApoyoMunicipalSolicitado6" maxlength="20"	placeholder=" $250,000" > </td>
+		<td><input type="text" name="AportacionBeneficiario6" maxlength="20" 	placeholder=" $250,000" > </td>
+		
 	</tr>
 
 
 
 </table>
 
-<input type="submit" name="EnviarConceptosPF" value="Enviar Información">
+<input type="submit" name="EnviarConceptosPF" value="Enviar Información" class="boton">
 
 </form>
 
 
 
 <h5>"Este programa es público; ajeno a cualquier partido político. Queda prohibido el uso para fines distinto a los establecidos al Programa"</h5>
+
+</div>
 </body>
 </html>
