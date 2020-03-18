a0196cecc801ba55661c1c09a015c0b9bb4dc270
landryzKnight <60994276+landryzKnight@users.noreply.github.com>
2020-03-18 11:29:18 -0600

    18/03

---
+++ mejoras.php
@@ -0,0 +1,69 @@
+
+
+
+banner en reportes
+
+color blanco y negro
+
+seccionar los reportes  15 reportes * pagina 
+
+
+
+boton posponer reportes, borrar cada reporte
+
+250 de varchar en para reporte de falla y mas detalles 
+
+al generar el reporte que de el numero de id del reporte
+ver estatus del reporte para el usuario
+
+
+poner diferentes tipos de letras 
+
+
+departamentos -> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cambiar la variable contraseña en iniciar sesion
+aplicar utf8
