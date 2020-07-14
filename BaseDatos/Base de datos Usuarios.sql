a74dac4509be3f5628be39d6eea27a55ab67181a
landryzKnight <60994276+landryzKnight@users.noreply.github.com>
2020-07-13 23:25:15 -0500

    13/07

--- BaseDatos/Base de datos Usuarios.sql
+++ BaseDatos/Base de datos Usuarios.sql
@@ -1,36 +1,44 @@
-#Base de Datos  Usuarios Sedea
+
+
+truncate Table usuarios;
+truncate Table usuariosReg;
 
 
 
-Create Table Usuarios(
+
+
+
+
+Create Table usuarios(
 idUsuario int Primary Key auto_increment,
 nameUsuario varchar(50),
-constrasenaUsuario varchar(50)
+pazz varchar(50)
 
 );
+
+
+
+Create Table usuariosReg(
+idUsuario int Primary Key auto_increment,
+nameUsuario varchar(50),
+pazz varchar(50)
+
+);
+
+
 
 insert into Usuarios values(null,'adminSda','sdaAdmin@_@2020');
-
-Create database SedeaUsuarios;
-
-
-
-Create Table UsuariosReg(
-idUsuario int Primary Key auto_increment,
-nameUsuario varchar(50),
-constrasenaUsuario varchar(50)
-
-);
-
+insert into Usuarios values(null,'juliosede@','sdeaingjulio_2020');
 insert into Usuarios values(null,'sedea','sde@_@2020');
+
 
 #idUsuario 1 AdminSda
 #idUsuario 2 Sedea
 #idUsuario 3 - 6 SedeaRegional
 
 
-insert into UsuariosReg values(null,'sedeaCadereyta','C@dereyt@sede@2020';
-insert into UsuariosReg values(null,'sedeaJalpan','J@lp@nsede@2020')
+insert into UsuariosReg values(null,'sedeaCadereyta','C@dereyt@sede@2020');
+insert into UsuariosReg values(null,'sedeaJalpan','J@lp@nsede@2020');
 insert into UsuariosReg values(null,'sedeaQro','Qrosede@2020');
 insert into UsuariosReg values(null,'sedeaSanJuan','S@nJu@nsede@2020');
 
---
+++ iniciosesionpm.php
@@ -0,0 +1,67 @@
+<?php 
+require_once('ConexionPF.php');
+
+
+global $user;
+global $pazz;
+
+if(isset($_POST['sesion'])){
+
+
+	$user = sanitizeString($_POST['usuarios']);
+	$pazz = sanitizeString($_POST['contras']);
+
+}
+
+		$select = "SELECT nameUsuario,pazz FROM usuarios WHERE nameUsuario = '{$user}' AND pazz = '{$pazz}'";
+
+		$result = queryMySql("$select");
+
+
+	
+	if($result -> num_rows){
+		
+		header('location: dictaminarpf.php');
+
+	}else {
+
+		echo "<input class='mensaje' type='text' name='mensaje' value='Usuario/Contraseña incorrectos.'>";
+	}
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio de Sesión</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estiloFisica.css">
+</head>
+<body>
+
+<div class="login" align="center">
+<h1>SEDEA Regional</h1>
+<h2>Ingresa los datos para <br>el inicio de sesión: </h2>
+
+
+
+<form action="iniciosesion.php" method="post">
+<h3>Usuario: </h3>
+<input type="text" name="usuarios" placeholder="Usuario SEDEA" required="required" maxlength="21" class="input">
+<h3>Contraseña: </h3>
+<input type="password" name="contras" placeholder="Contraseña SEDEA" required="required" maxlength="21" class="input">
+
+<br>
+<br>
+<input type="submit" name="sesion" value="Iniciar sesión" class="boton"></a>
+
+<br><br>
+<a href="http://localhost/sedea/index.php"><button class="boton">Menú Principal</button>
+ </form>
+</div>
+
+
+</body>
+</html>
